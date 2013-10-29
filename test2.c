1c9e60b20e1208d0596d1829b2486a584e89392b
visper <zzzvisperzzz@yandex.ru>
2013-10-29 17:07:31 +0200

    worked test, P.S. CU_FAIL & CU_PASS worked equal when we use "" or not use

--- test2.c
+++ test2.c
@@ -17,40 +17,38 @@ int foo(void)
 
 void input_int(void)//t1
 {
-  //  CU_ASSERT_EQUAL(1,1);
   //assert for int overflow int 2 147 483 647
-  char str[10] = {'0','0','0','0','0','0','0','0','0','0'};
+  char str[] = {'0','0','0','0','0','0','0','0','0','0'};
   printf("enter int\n");
   scanf("%s",str);
 
   int len = strlen(str);
-  if(len > 10) CU_FAIL(input overflow!!);
+  if(len > 10) CU_FAIL("input overflow!!");
+
+  if(str[0] == '-') CU_FAIL("signed number");
 
   //check if input nothing or "0"
-  int num_count = 0;//I can use "i",and not use "num_count" but work is wrong
-  for(int i = 0; i < 11; i++){
+  int num_count = 0;
+  int in_zero = ((sizeof(str))/(sizeof(str[0]))) - 1;
+  for(int i = 0; i < ((sizeof(str))/(sizeof(str[0]))); i++){
     if(str[i] == '0') ++num_count;
-    if(num_count == 10){
-      CU_FAIL(nothing wasn`t input);
+    if(num_count == in_zero){
+      CU_FAIL("nothing wasn`t input");
     }
   }
-
-  char str_expected[10] = {'2','1','4','7','4','8','3','6','4','7'};
-  CU_ASSERT_STRING_NOT_EQUAL(str,str_expected);
-  /*it`s boundary value. at this moment we must understand our value more or less this value but user can input not int value or not number value*/
 
   //input must be a number
   bool done = 0;
   int counts = 0;
-  char nums[11] = {'0','1','2','3','4','5','6','7','8','9',','};
-  for(int i = 0; i < 10; i++){
+  char nums[] = {'0','1','2','3','4','5','6','7','8','9',','};
+  for(int i = 0; i < ((sizeof(str))/(sizeof(str[0]))); i++){
     if(done) break;
     counts = 0;
-    for (int j = 0; j < 11; j++){
+    for (int j = 0; j < ((sizeof(nums))/(sizeof(nums[0]))); j++){
       if(str[i] != nums[j]){
 	++counts;
-	if(counts == 10){
-	  CU_FAIL(ERROR_INPUT input is not a number!!!!);
+	if(counts == len){
+	  CU_FAIL("ERROR_INPUT input is not a number!!!!");
 	  //out from loop because ^^
 	  done = true;
 	  break;//must return 1
@@ -58,97 +56,87 @@ void input_int(void)//t1
       }
       else{
 	done = true;
-	CU_PASS(input is a number);
+	CU_PASS("input is a number");
 	break;
       }
     }
   }
   
   //if double
-  //  int iStr[11] = {0,0,0,0,0,0,0,0,0,0,0};
   bool ddouble = 0;
   int elmass_coma = 0;
   
-  for(int i = 0; i < 11; i++){
+  for(int i = 0; i < ((sizeof(str))/(sizeof(str[0]))); i++){
     if(str[i] == ',' || str[i] == '.'){
-      //CU_PASS(input is double it will be converted);
+      //this line not printed
+      CU_PASS("input is double it will be converted");
       elmass_coma = i;
       ddouble = 1;
       break;
     }
   }
   
-  if(elmass_coma == 10) CU_FAIL(input double number more then integer);
+  if(elmass_coma == 10) CU_FAIL("input double number more then integer");
 
   int input_num = 0;
   if(ddouble){
-    //    for(int i = 0; i < elmass_coma; i++){
-    //iStr[i] = str[i];
-    //}
     input_num = atoi(str);
     printf("input double converted in int = %i\n", input_num);
-    //if(elmass_coma < 11){
-    //iStr[11] = 0;
-    //CU_PASS(enter number less then length of integer);
-    //break; must out
-    //}
   }
   else{    //if integer
-    //    CU_ASSERT_EQUAL(str[10], '0');
-    int count_nums_nZ = 0;
-    for(int i = 0; i < 10; i++){
-      if(str[i] != 0) count_nums_nZ++;
-    }
-    
-    CU_ASSERT_NOT_EQUAL(count_nums_nZ,11);
-    if(count_nums_nZ == 10){
-      int condition = 0;//2 147 483 647
-      if(str[0] > '2') condition++;
-      if(str[1] > '1') condition++;
-      if(str[2] > '4') condition++;
-      if(str[3] > '7') condition++;
-      if(str[4] > '4') condition++;
-      if(str[5] > '8') condition++;
-      if(str[6] > '3') condition++;
-      if(str[7] > '6') condition++;
-      if(str[8] > '4') condition++;
-      if(str[9] > '7') condition++;
-      
-      if(condition == 10) CU_FAIL(integer overflow!!);
-    }
-    else{
-      CU_PASS(input integer less then 2147483647);
-      //for(int i = 0; i < 11; i++){
+    if(len == 10){
+      for(int i = 0; i < len; i++){
+	if(str[0] > '2'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+	if(str[1] > '1'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+	if(str[2] > '4'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+	if(str[3] > '7'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+	if(str[4] > '4'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+	if(str[5] > '8'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+	if(str[6] > '3'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+	if(str[7] > '6'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+	if(str[8] > '4'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+	if(str[9] > '7'){
+	  CU_FAIL("int type is overflow!");
+	  break;
+	}
+      }//END FOR
+      //if loop not break then input boundary value
       input_num = atoi(str);
       printf("input int = %i\n", input_num);
     }
-    //}
+    else{
+      CU_PASS("input integer less then 2147483647");
+      input_num = atoi(str);
+      printf("input int = %i\n", input_num);
+    }
   }
-  //___________________________________________________________
-  //**************************************************
-  //___________________________________________________________
-
-  /*  //number must be input
-  for(int i = 0; i < 11; i++){
-    CU_ASSERT_NOT_EQUAL(str[i],0);
-    CU_FAIL(no number);
-    CU_ASSERT_NOT_EQUAL(i,13);
-    }*/
-
-  /*  //number must be input
-  for(int i = 0; i < 11; i++){
-    CU_ASSERT_NOT_EQUAL(str[i],0);
-    CU_FAIL(no number);
-    CU_ASSERT_NOT_EQUAL(i,13);
-  }
-
-  static int count = 0;
-  for(int i = 0; i < 11; i++){
-    ++count;
-    CU_ASSERT_NOT_EQUAL(i,13);
-  }*/
-
-
 }
 
 void t2(void)
